b18190be8fe3aa73c0409a15506ecc29a4a4a165
Gary Gisclair <gary@garygisclair.com>
2012-05-12 17:39:24 -0500

    Added Math to make columns equal spans

--- components/com_content/views/category/tmpl/blog.php
+++ components/com_content/views/category/tmpl/blog.php
@@ -74,9 +74,9 @@ JHtml::addIncludePath(JPATH_COMPONENT.'/helpers');
 		$row = $counter / $this->columns ;
 
 		if ($rowcount==1) : ?>
-	<div class="items-row cols-<?php echo (int) $this->columns;?> <?php echo 'row-'.$row ; ?>">
+	<div class="items-row cols-<?php echo (int) $this->columns;?> <?php echo 'row-'.$row ; ?> row-fluid">
 	<?php endif; ?>
-	<div class="item column-<?php echo $rowcount;?><?php echo $item->state == 0 ? ' system-unpublished' : null; ?>">
+	<div class="item column-<?php echo $rowcount;?><?php echo $item->state == 0 ? ' system-unpublished' : null; ?> span<?php echo round((12/$this->columns));?>">
 		<?php
 			$this->item = &$item;
 			echo $this->loadTemplate('item');
