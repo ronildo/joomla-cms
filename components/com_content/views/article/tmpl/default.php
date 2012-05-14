0f9388ddd1b9b4766315eb7c93364a29d31e4bc3
Gary Gisclair <gary@garygisclair.com>
2012-05-14 09:12:25 -0500

    Formatted file with tab spacing

--- components/com_content/views/article/tmpl/default.php
+++ components/com_content/views/article/tmpl/default.php
@@ -19,190 +19,154 @@ $canEdit	= $this->item->params->get('access-edit');
 $user		= JFactory::getUser();
 
 ?>
+
 <div class="item-page<?php echo $this->pageclass_sfx?>">
-<?php if ($this->params->get('show_page_heading', 1)) : ?>
-<div class="page-header">
-	<h1>
-	<?php echo $this->escape($this->params->get('page_heading')); ?>
-	</h1>
-</div>
-<?php endif; ?>
-<?php
+	<?php if ($this->params->get('show_page_heading', 1)) : ?>
+	<div class="page-header">
+		<h1> <?php echo $this->escape($this->params->get('page_heading')); ?> </h1>
+	</div>
+	<?php endif; ?>
+	<?php
 if (!empty($this->item->pagination) AND $this->item->pagination && !$this->item->paginationposition && $this->item->paginationrelative)
 {
  echo $this->item->pagination;
 }
  ?>
-<?php if ($canEdit ||  $params->get('show_print_icon') || $params->get('show_email_icon')) : ?>
-<div class="btn-group pull-right">	
-	<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
-	    <i class="icon-cog"></i>
-	    <span class="caret"></span>
-	  </a>
-	<ul class="dropdown-menu">
-		<?php if (!$this->print) : ?>
-		<?php if ($params->get('show_print_icon')) : ?>
-			<li class="print-icon">
-			<?php echo JHtml::_('icon.print_popup',  $this->item, $params); ?>
-			</li>
-		<?php endif; ?>
-	
-		<?php if ($params->get('show_email_icon')) : ?>
-			<li class="email-icon">
-			<?php echo JHtml::_('icon.email',  $this->item, $params); ?>
-			</li>
-		<?php endif; ?>
-	
-		<?php if ($canEdit) : ?>
-			<li class="edit-icon">
-			<?php echo JHtml::_('icon.edit', $this->item, $params); ?>
-			</li>
-		<?php endif; ?>
-		<?php else : ?>
-		<li>
-		<?php echo JHtml::_('icon.print_screen',  $this->item, $params); ?>
-		</li>
-		<?php endif; ?>
-	</ul>
-</div>
-<?php endif; ?>
-<?php if ($params->get('show_title')) : ?>
-<div class="page-header">
-	<h2>
-	<?php if ($params->get('link_titles') && !empty($this->item->readmore_link)) : ?>
-		<a href="<?php echo $this->item->readmore_link; ?>">
-		<?php echo $this->escape($this->item->title); ?></a>
-	<?php else : ?>
-		<?php echo $this->escape($this->item->title); ?>
+	<?php if ($canEdit ||  $params->get('show_print_icon') || $params->get('show_email_icon')) : ?>
+	<div class="btn-group pull-right"> <a class="btn dropdown-toggle" data-toggle="dropdown" href="#"> <i class="icon-cog"></i> <span class="caret"></span> </a>
+		<ul class="dropdown-menu">
+			<?php if (!$this->print) : ?>
+			<?php if ($params->get('show_print_icon')) : ?>
+			<li class="print-icon"> <?php echo JHtml::_('icon.print_popup',  $this->item, $params); ?> </li>
+			<?php endif; ?>
+			<?php if ($params->get('show_email_icon')) : ?>
+			<li class="email-icon"> <?php echo JHtml::_('icon.email',  $this->item, $params); ?> </li>
+			<?php endif; ?>
+			<?php if ($canEdit) : ?>
+			<li class="edit-icon"> <?php echo JHtml::_('icon.edit', $this->item, $params); ?> </li>
+			<?php endif; ?>
+			<?php else : ?>
+			<li> <?php echo JHtml::_('icon.print_screen',  $this->item, $params); ?> </li>
+			<?php endif; ?>
+		</ul>
+	</div>
 	<?php endif; ?>
-	</h2>
-</div>
-<?php endif; ?>
-<?php  if (!$params->get('show_intro')) : echo $this->item->event->afterDisplayTitle; endif; ?>
-<?php echo $this->item->event->beforeDisplayContent; ?>
-<?php $useDefList = (($params->get('show_author')) or ($params->get('show_category')) or ($params->get('show_parent_category'))
+	<?php if ($params->get('show_title')) : ?>
+	<div class="page-header">
+		<h2>
+			<?php if ($params->get('link_titles') && !empty($this->item->readmore_link)) : ?>
+			<a href="<?php echo $this->item->readmore_link; ?>"> <?php echo $this->escape($this->item->title); ?></a>
+			<?php else : ?>
+			<?php echo $this->escape($this->item->title); ?>
+			<?php endif; ?>
+		</h2>
+	</div>
+	<?php endif; ?>
+	<?php  if (!$params->get('show_intro')) : echo $this->item->event->afterDisplayTitle; endif; ?>
+	<?php echo $this->item->event->beforeDisplayContent; ?>
+	<?php $useDefList = (($params->get('show_author')) or ($params->get('show_category')) or ($params->get('show_parent_category'))
 	or ($params->get('show_create_date')) or ($params->get('show_modify_date')) or ($params->get('show_publish_date'))
 	or ($params->get('show_hits'))); ?>
 	<?php if ($useDefList) : ?>
 	<div class="article-info well">
 		<ul class="unstyled">
-			<li class="article-info-term"><strong><?php  echo JText::_('COM_CONTENT_ARTICLE_INFO'); ?></strong></li>
-	<?php endif; ?>
-<?php if ($params->get('show_parent_category') && $this->item->parent_slug != '1:root') : ?>
-	<li class="parent-category-name">
-	<i class="icon-folder-open"></i> 
-	<?php	$title = $this->escape($this->item->parent_title);
+			<li class="article-info-term"><strong>
+				<?php  echo JText::_('COM_CONTENT_ARTICLE_INFO'); ?>
+				</strong></li>
+			<?php endif; ?>
+			<?php if ($params->get('show_parent_category') && $this->item->parent_slug != '1:root') : ?>
+			<li class="parent-category-name"> <i class="icon-folder-open"></i>
+				<?php	$title = $this->escape($this->item->parent_title);
 	$url = '<a href="'.JRoute::_(ContentHelperRoute::getCategoryRoute($this->item->parent_slug)).'">'.$title.'</a>';?>
-	<?php if ($params->get('link_parent_category') and $this->item->parent_slug) : ?>
-		<?php echo JText::sprintf('COM_CONTENT_PARENT', $url); ?>
-	<?php else : ?>
-		<?php echo JText::sprintf('COM_CONTENT_PARENT', $title); ?>
-	<?php endif; ?>
-	</li>
-<?php endif; ?>
-<?php if ($params->get('show_category')) : ?>
-	<li class="category-name">
-	<i class="icon-folder-open"></i> 
-	<?php 	$title = $this->escape($this->item->category_title);
+				<?php if ($params->get('link_parent_category') and $this->item->parent_slug) : ?>
+				<?php echo JText::sprintf('COM_CONTENT_PARENT', $url); ?>
+				<?php else : ?>
+				<?php echo JText::sprintf('COM_CONTENT_PARENT', $title); ?>
+				<?php endif; ?>
+			</li>
+			<?php endif; ?>
+			<?php if ($params->get('show_category')) : ?>
+			<li class="category-name"> <i class="icon-folder-open"></i>
+				<?php 	$title = $this->escape($this->item->category_title);
 	$url = '<a href="'.JRoute::_(ContentHelperRoute::getCategoryRoute($this->item->catslug)).'">'.$title.'</a>';?>
-	<?php if ($params->get('link_category') and $this->item->catslug) : ?>
-		<?php echo JText::sprintf('COM_CONTENT_CATEGORY', $url); ?>
-	<?php else : ?>
-		<?php echo JText::sprintf('COM_CONTENT_CATEGORY', $title); ?>
-	<?php endif; ?>
-	</li>
-<?php endif; ?>
-<?php if ($params->get('show_create_date')) : ?>
-	<li class="create">
-	<i class="icon-calendar"></i> 
-	<?php echo JText::sprintf('COM_CONTENT_CREATED_DATE_ON', JHtml::_('date', $this->item->created, JText::_('DATE_FORMAT_LC2'))); ?>
-	</li>
-<?php endif; ?>
-<?php if ($params->get('show_modify_date')) : ?>
-	<li class="modified">
-	<i class="icon-calendar"></i> 
-	<?php echo JText::sprintf('COM_CONTENT_LAST_UPDATED', JHtml::_('date', $this->item->modified, JText::_('DATE_FORMAT_LC2'))); ?>
-	</li>
-<?php endif; ?>
-<?php if ($params->get('show_publish_date')) : ?>
-	<li class="published">
-	<i class="icon-calendar"></i> 
-	<?php echo JText::sprintf('COM_CONTENT_PUBLISHED_DATE_ON', JHtml::_('date', $this->item->publish_up, JText::_('DATE_FORMAT_LC2'))); ?>
-	</li>
-<?php endif; ?>
-<?php if ($params->get('show_author') && !empty($this->item->author )) : ?>
-	<li class="createdby">
-	<i class="icon-user"></i> 
-	<?php $author = $this->item->created_by_alias ? $this->item->created_by_alias : $this->item->author; ?>
-	<?php if (!empty($this->item->contactid) && $params->get('link_author') == true): ?>
-	<?php
+				<?php if ($params->get('link_category') and $this->item->catslug) : ?>
+				<?php echo JText::sprintf('COM_CONTENT_CATEGORY', $url); ?>
+				<?php else : ?>
+				<?php echo JText::sprintf('COM_CONTENT_CATEGORY', $title); ?>
+				<?php endif; ?>
+			</li>
+			<?php endif; ?>
+			<?php if ($params->get('show_create_date')) : ?>
+			<li class="create"> <i class="icon-calendar"></i> <?php echo JText::sprintf('COM_CONTENT_CREATED_DATE_ON', JHtml::_('date', $this->item->created, JText::_('DATE_FORMAT_LC2'))); ?> </li>
+			<?php endif; ?>
+			<?php if ($params->get('show_modify_date')) : ?>
+			<li class="modified"> <i class="icon-calendar"></i> <?php echo JText::sprintf('COM_CONTENT_LAST_UPDATED', JHtml::_('date', $this->item->modified, JText::_('DATE_FORMAT_LC2'))); ?> </li>
+			<?php endif; ?>
+			<?php if ($params->get('show_publish_date')) : ?>
+			<li class="published"> <i class="icon-calendar"></i> <?php echo JText::sprintf('COM_CONTENT_PUBLISHED_DATE_ON', JHtml::_('date', $this->item->publish_up, JText::_('DATE_FORMAT_LC2'))); ?> </li>
+			<?php endif; ?>
+			<?php if ($params->get('show_author') && !empty($this->item->author )) : ?>
+			<li class="createdby"> <i class="icon-user"></i>
+				<?php $author = $this->item->created_by_alias ? $this->item->created_by_alias : $this->item->author; ?>
+				<?php if (!empty($this->item->contactid) && $params->get('link_author') == true): ?>
+				<?php
 		$needle = 'index.php?option=com_contact&view=contact&id=' . $this->item->contactid;
 		$menu = JFactory::getApplication()->getMenu();
 		$item = $menu->getItems('link', $needle, true);
 		$cntlink = !empty($item) ? $needle . '&Itemid=' . $item->id : $needle;
 	?>
-		<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', JHtml::_('link', JRoute::_($cntlink), $author)); ?>
-	<?php else: ?>
-		<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', $author); ?>
-	<?php endif; ?>
-	</li>
-<?php endif; ?>
-<?php if ($params->get('show_hits')) : ?>
-	<li class="hits">
-	<i class="icon-eye-open"></i> 
-	<?php echo JText::sprintf('COM_CONTENT_ARTICLE_HITS', $this->item->hits); ?>
-	</li>
-<?php endif; ?>
-<?php if ($useDefList) : ?>
-	</ul>
+				<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', JHtml::_('link', JRoute::_($cntlink), $author)); ?>
+				<?php else: ?>
+				<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', $author); ?>
+				<?php endif; ?>
+			</li>
+			<?php endif; ?>
+			<?php if ($params->get('show_hits')) : ?>
+			<li class="hits"> <i class="icon-eye-open"></i> <?php echo JText::sprintf('COM_CONTENT_ARTICLE_HITS', $this->item->hits); ?> </li>
+			<?php endif; ?>
+			<?php if ($useDefList) : ?>
+		</ul>
 	</div>
-<?php endif; ?>
-
-<?php if (isset ($this->item->toc)) : ?>
+	<?php endif; ?>
+	<?php if (isset ($this->item->toc)) : ?>
 	<?php echo $this->item->toc; ?>
-<?php endif; ?>
-
-<?php if (isset($urls) AND ((!empty($urls->urls_position) AND ($urls->urls_position=='0')) OR  ($params->get('urls_position')=='0' AND empty($urls->urls_position) ))
+	<?php endif; ?>
+	<?php if (isset($urls) AND ((!empty($urls->urls_position) AND ($urls->urls_position=='0')) OR  ($params->get('urls_position')=='0' AND empty($urls->urls_position) ))
 		OR (empty($urls->urls_position) AND (!$params->get('urls_position')))): ?>
-<?php echo $this->loadTemplate('links'); ?>
-<?php endif; ?>
-
-<?php if ($params->get('access-view')):?>
-<?php  if (isset($images->image_fulltext) and !empty($images->image_fulltext)) : ?>
-<?php $imgfloat = (empty($images->float_fulltext)) ? $params->get('float_fulltext') : $images->float_fulltext; ?>
-<div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image">
-<img
+	<?php echo $this->loadTemplate('links'); ?>
+	<?php endif; ?>
+	<?php if ($params->get('access-view')):?>
+	<?php  if (isset($images->image_fulltext) and !empty($images->image_fulltext)) : ?>
+	<?php $imgfloat = (empty($images->float_fulltext)) ? $params->get('float_fulltext') : $images->float_fulltext; ?>
+	<div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image"> <img
 	<?php if ($images->image_fulltext_caption):
 		echo 'class="caption"'.' title="' .htmlspecialchars($images->image_fulltext_caption) .'"';
 	endif; ?>
-	src="<?php echo htmlspecialchars($images->image_fulltext); ?>" alt="<?php echo htmlspecialchars($images->image_fulltext_alt); ?>"/>
-</div>
-<?php endif; ?>
-<?php
+	src="<?php echo htmlspecialchars($images->image_fulltext); ?>" alt="<?php echo htmlspecialchars($images->image_fulltext_alt); ?>"/> </div>
+	<?php endif; ?>
+	<?php
 if (!empty($this->item->pagination) AND $this->item->pagination AND !$this->item->paginationposition AND !$this->item->paginationrelative):
 	echo $this->item->pagination;
  endif;
 ?>
-<?php echo $this->item->text; ?>
-<?php
+	<?php echo $this->item->text; ?>
+	<?php
 if (!empty($this->item->pagination) AND $this->item->pagination AND $this->item->paginationposition AND!$this->item->paginationrelative):
 	echo '<div class="clearfix"></div>';
 	echo $this->item->pagination;
 ?>
-<?php endif; ?>
-
-<?php if (isset($urls) AND ((!empty($urls->urls_position)  AND ($urls->urls_position=='1')) OR ( $params->get('urls_position')=='1') )): ?>
-<?php echo $this->loadTemplate('links'); ?>
-<?php endif; ?>
+	<?php endif; ?>
+	<?php if (isset($urls) AND ((!empty($urls->urls_position)  AND ($urls->urls_position=='1')) OR ( $params->get('urls_position')=='1') )): ?>
+	<?php echo $this->loadTemplate('links'); ?>
+	<?php endif; ?>
 	<?php //optional teaser intro text for guests ?>
-<?php elseif ($params->get('show_noauth') == true and  $user->get('guest') ) : ?>
+	<?php elseif ($params->get('show_noauth') == true and  $user->get('guest') ) : ?>
 	<?php echo $this->item->introtext; ?>
 	<?php //Optional link to let them register to see the whole article. ?>
 	<?php if ($params->get('show_readmore') && $this->item->fulltext != null) :
 		$link1 = JRoute::_('index.php?option=com_users&view=login');
 		$link = new JURI($link1);?>
-		<p class="readmore">
-		<a href="<?php echo $link; ?>">
+	<p class="readmore"> <a href="<?php echo $link; ?>">
 		<?php $attribs = json_decode($this->item->attribs);  ?>
 		<?php
 		if ($attribs->alternative_readmore == null) :
@@ -217,14 +181,13 @@ if (!empty($this->item->pagination) AND $this->item->pagination AND $this->item-
 		else :
 			echo JText::_('COM_CONTENT_READ_MORE');
 			echo JHtml::_('string.truncate', ($this->item->title), $params->get('readmore_limit'));
-		endif; ?></a>
-		</p>
+		endif; ?>
+		</a> </p>
 	<?php endif; ?>
-<?php endif; ?>
-<?php
+	<?php endif; ?>
+	<?php
 if (!empty($this->item->pagination) AND $this->item->pagination AND $this->item->paginationposition AND $this->item->paginationrelative):
 	echo $this->item->pagination;
 ?>
-<?php endif; ?>
-<?php echo $this->item->event->afterDisplayContent; ?>
-</div>
+	<?php endif; ?>
+	<?php echo $this->item->event->afterDisplayContent; ?> </div>
