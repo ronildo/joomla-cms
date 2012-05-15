10dc11746dad8bad6113680b4715e0c73a6629ad
Kyle Ledbetter <kyle@kyleledbetter.com>
2012-05-14 20:57:04 -0500

    - removing unnecessary height javascript (application.js not currently used)
    Signed-off-by: Kyle Ledbetter <kyle@kyleledbetter.com>

--- administrator/templates/strapped/js/application.js
+++ administrator/templates/strapped/js/application.js
@@ -5,8 +5,3 @@ $('.typeahead').typeahead()
 $('.tabs').button()
 $('.tip').tooltip()
 $(".alert-message").alert()
-$(window).bind("load resize", function() {
- var windowHeight = "height:"+($(window).height()-45)+"px"; // height of full document
- var windowWidth = "width:"+($(window).width()-30)+"px"; // width of full document
- $('.side-nav, .fluid-content.main, .modal.full').attr('style', windowHeight);
-});
