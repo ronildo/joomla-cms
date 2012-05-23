4775006d941180f5c7f35981d8967825b031f012
Kyle Ledbetter <kyle@kyleledbetter.com>
2012-05-23 10:37:29 -0500

    - adding mobile viewport default to login
    Signed-off-by: Kyle Ledbetter <kyle@kyleledbetter.com>

--- administrator/templates/strapped/login.php
+++ administrator/templates/strapped/login.php
@@ -48,6 +48,7 @@ $doc->addStyleSheet('templates/'.$this->template.'/css/template.css');
     	$span = "span12";
     endif;
 	?>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template ?>/js/jquery.js"></script>
 	<script src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template ?>/js/bootstrap.min.js"></script>
 	<script type="text/javascript">
