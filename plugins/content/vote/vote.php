1db956db1925ee45961cc8b349b31a09aee45ad3
Kyle Ledbetter <kyle@kyleledbetter.com>
2012-05-25 11:13:27 -0500

    - commenting out "poor" and "best", changing submit to btn-mini
    Signed-off-by: Kyle Ledbetter <kyle@kyleledbetter.com>

--- plugins/content/vote/vote.php
+++ plugins/content/vote/vote.php
@@ -55,7 +55,7 @@ class plgContentVote extends JPlugin
 				$img .= $starImageOff;
 			}
 			$html .= '<span class="content_rating">';
-			$html .= JText::sprintf( 'PLG_VOTE_USER_RATING', $img, $rating_count );
+			$html .= JText::sprintf( $img );
 
 			if ( $view == 'article' && $row->state == 1)
 			{
@@ -64,14 +64,14 @@ class plgContentVote extends JPlugin
 
 				$html .= '<form method="post" action="' . $uri->toString() . '">';
 				$html .= '<span class="content_vote">';
-				$html .= JText::_( 'PLG_VOTE_POOR' );
+//				$html .= JText::_( 'PLG_VOTE_POOR' );
 				$html .= ' <input type="radio" title="'.JText::sprintf('PLG_VOTE_VOTE', '1').'" name="user_rating" value="1" /> ';
 				$html .= '<input type="radio" title="'.JText::sprintf('PLG_VOTE_VOTE', '2').'" name="user_rating" value="2" /> ';
 				$html .= '<input type="radio" title="'.JText::sprintf('PLG_VOTE_VOTE', '3').'" name="user_rating" value="3" /> ';
 				$html .= '<input type="radio" title="'.JText::sprintf('PLG_VOTE_VOTE', '4').'" name="user_rating" value="4" /> ';
 				$html .= '<input type="radio" title="'.JText::sprintf('PLG_VOTE_VOTE', '5').'" name="user_rating" value="5" checked="checked" /> ';
-				$html .= JText::_( 'PLG_VOTE_BEST' );
-				$html .= '&#160;<input class="btn btn-small" type="submit" name="submit_vote" value="'. JText::_( 'PLG_VOTE_RATE' ) .'" />';
+//				$html .= JText::_( 'PLG_VOTE_BEST' );
+				$html .= '&#160;<input class="btn btn-mini" type="submit" name="submit_vote" value="'. JText::_( 'PLG_VOTE_RATE' ) .'" />';
 				$html .= '<input type="hidden" name="task" value="article.vote" />';
 				$html .= '<input type="hidden" name="hitcount" value="0" />';
 				$html .= '<input type="hidden" name="url" value="'.  $uri->toString() .'" />';
