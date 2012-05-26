0a638a8d7303f875500f0f16695da03bedbf214f
Kyle Ledbetter <kledbetter@ebay.com>
2012-05-26 13:27:06 -0500

    - using input-prepend for username remind and password reset
    Signed-off-by: Kyle Ledbetter <kledbetter@ebay.com>

--- administrator/modules/mod_login/tmpl/default.php
+++ administrator/modules/mod_login/tmpl/default.php
@@ -14,17 +14,17 @@ JHtml::_('behavior.keepalive');
 	<fieldset class="loginform">
 		<div class="control-group">
 			<div class="controls">
-			  <div class="input-prepend">
-			    <span class="add-on"><i class="icon-user"></i></span><input name="username" id="mod-login-username" type="text" class="inputbox tip" title="<?php echo JText::_('JGLOBAL_USERNAME'); ?>" size="15" />
+			  <div class="input-prepend input-append">
+			    <span class="add-on"><i class="icon-user"></i></span><input name="username" id="mod-login-username" type="text" class="input-medium tip" title="<?php echo JText::_('JGLOBAL_USERNAME'); ?>" size="15" /><a href="<?php echo JURI::root()?>index.php?option=com_users&view=remind" class="btn width-50"><?php echo JText::_('MOD_LOGIN_REMIND'); ?></a>
 			  </div>
 			</div>
 		</div>
 		<div class="control-group">
 			<div class="controls">
-			  <div class="input-prepend">
-			    <span class="add-on"><i class="icon-lock"></i></span><input name="passwd" id="mod-login-password" type="password" class="inputbox tip" title="<?php echo JText::_('JGLOBAL_PASSWORD'); ?>" size="15" />
+			  <div class="input-prepend input-append">
+			    <span class="add-on"><i class="icon-lock"></i></span><input name="passwd" id="mod-login-password" type="password" class="input-medium tip" title="<?php echo JText::_('JGLOBAL_PASSWORD'); ?>" size="15" /><a href="<?php echo JURI::root()?>index.php?option=com_users&view=reset" class="btn width-50"><?php echo JText::_('MOD_LOGIN_RESET'); ?></a>
 			  </div>
-			</div>
+			 </div>
 		</div>
 		<div class="control-group">
 			<div class="controls">
